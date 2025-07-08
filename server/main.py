0575bf216f9ebb517381d0496a86be83c5a1c47a
Tanishq <tanishqtomar6969@gmail.com>
2025-07-08 17:47:30 +0530

    Implemented CORS to fastapi

--- server/main.py
+++ server/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from transformers import pipeline
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:5173'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 sentiment_analysis=pipeline("text-classification",model="boltuix/bert-emotion")
 
